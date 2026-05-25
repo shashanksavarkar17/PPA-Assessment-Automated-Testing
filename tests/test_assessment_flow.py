a7bd91dc545246ea4e02a459701ea0e9b9039075
shashanksavarkar17 <savarkar.shashank_btech24@gsv.ac.in>
2026-05-25 13:08:10 +0530

    refactor: remove legacy artifacts and initialize new assessment flow test suite

--- tests/test_assessment_flow.py
+++ tests/test_assessment_flow.py
@@ -1,8 +1,6 @@
 # Hey friend! You can use this file if you ever decide to run your tests via pytest.
 # For now, we are running our orchestrator directly using: python main.py
 
-from utils.driver_factory import get_chrome_driver
-from config import settings
 
 # This placeholder test is here to keep pytest happy in the CI pipeline.
 def test_placeholder():
